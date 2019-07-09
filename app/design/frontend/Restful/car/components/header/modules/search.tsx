d3d18c4359267293c4877fa7966bfe4d51258355
caesar <454451758@qq.com>
2019-07-09 08:43:20 +0800

    Cart page update

--- app/design/frontend/Restful/car/components/header/modules/search.tsx
+++ app/design/frontend/Restful/car/components/header/modules/search.tsx
@@ -5,13 +5,18 @@ import { VAdvanceSearch } from './advance'
 @Component({
     name: 'v-search',
     data: () => ({
-        title: 'search'
+        i18n: {
+            search: 'Search entire store here'
+        },
+        search: ''
     }),
     components: {
         VAdvanceSearch
     }
 })
 export class VSearch extends Vue {
+    public search: string = ''
+    
     mounted () {
         this.init()
     }
